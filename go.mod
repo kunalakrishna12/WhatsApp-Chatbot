9fd054ae08dcba678e422ae70e8593d60119abaf
Satya Venkata Sri Krishna Kunala <64822404+kunalakrishna12@users.noreply.github.com>
2024-07-25 22:08:56 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,29 @@
+module example.com/m
+
+go 1.21
+
+toolchain go1.22.3
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/mdp/qrterminal/v3 v3.2.0
+	go.mau.fi/whatsmeow v0.0.0-20240523075404-7f13c31d2cb1
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mdp/qrterminal v1.0.1 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	go.mau.fi/libsignal v0.1.0 // indirect
+	go.mau.fi/util v0.4.2 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
+)
+
+type MyClient struct {
+	WAClient       *whatsmeow.Client
+	eventHandlerID uint32
+	targetNumber   string
+}
+
+func (mycli *MyClient) register() {
+	mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.eventHandler)
+}
+
+func (mycli *MyClient) eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		newMessage := v.Message
+		msg := newMessage.GetConversation()
+		sender := v.Info.Sender.User
+
+		fmt.Println("Message from:", sender, "->", msg)
+		if msg == "" {
+			return
+		}
+
+		// Filter messages from a specific number
+		if sender != mycli.targetNumber {
+			fmt.Println("Ignoring message from:", sender)
+			return
+		}
+
+		// URL encode the message
+		urlEncoded := url.QueryEscape(msg)
+		apiURL := "http://localhost:5001/chat?q=" + urlEncoded
+		// Make the request
+		resp, err := http.Get(apiURL)
+		if err != nil {
+			fmt.Println("Error making request:", err)
+			return
+		}
+		defer resp.Body.Close()
+		// Read the response
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+		newMsg := buf.String()
+
+		// Prepare the response message
+		response := &waProto.Message{Conversation: proto.String(newMsg)}
+		fmt.Println("Response:", response)
+
+		userJid := types.NewJID(sender, types.DefaultUserServer)
+		mycli.WAClient.SendMessage(context.Background(), userJid, response)
+	}
+}
+
+func main() {
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+
+	// Specify the target number here (e.g., "1234567890")
+	targetNumber := "917288854090"
+
+	// Add the event handler
+	mycli := &MyClient{WAClient: client, targetNumber: targetNumber}
+	mycli.register()
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
